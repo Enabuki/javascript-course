db12cd31cf04c37aca4afba10113dd0ac7301d84
Andrei <aseronandrei14@gmail.com>
2025-09-10 13:10:42 +0800

    // Start as a number let dynamicVariable = 23; console.log(dynamicVariable, typeof dynamicVariable); // 23, "number"
    // Change to a string
    dynamicVariable = "Now I'm a string!";
    console.log(dynamicVariable, typeof dynamicVariable); // "Now I'm a string!", "string"
    
    // Change to a boolean
    dynamicVariable = true;
    console.log(dynamicVariable, typeof dynamicVariable); // true, "boolean"

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,64 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log(js);
+
+console.log("=== VARIABLES ===");
+
+
+
+let age = 30;
+console.log(age);
+age = 50;
+console.log(age);
+
+
+const birthYear = 1991;
+console.log(birthYear);
+
+
+const PI = 3.1415
+console.log(PI);
+
+var job = "programmer"
+job = "teacher"
+console.log(job);
+
+
+console.log("=== DATA TYPES ===");
+
+//Number
+let id = 12345;
+console.log(id);
+console.log(typeof id);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(typeof firstName); 
+
+// Boolean (true or false)
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+// Undefined (no value assigned yet)
+let year;
+console.log(year); 
+console.log(typeof year); 
+
+// Start as a number
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable); 
+
+// Change to a string
+dynamicVariable = "Now I'm a string!";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+// Change to a boolean
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable); 
+
+
